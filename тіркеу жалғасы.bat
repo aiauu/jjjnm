a86484b87b6049bfd8f200a44cfe020c2d2ba183
aiauu <11082006as@gmail.com>
2024-12-11 08:49:11 +0500

    Add files via upload

---
+++ тіркеу жалғасы.bat
@@ -0,0 +1,14 @@
+ <!DOCTYPE html>
+<html>
+    <head>
+        <title>тіркеу формасы</title>
+    </head>
+    <body>
+    <h2> <b> Registration Form</b></h2>  
+<form action="">
+ <label for ="name"> First Name :</label>
+<input type = "text" name = "name" placeholder = "First Name" id=" name">
+<br>
+<label for ="name"> Last Name :</label>
+<input type = "text" name = "name" placeholder = " Last name" id=" name">
+</form>
